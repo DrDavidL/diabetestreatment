dadcbbe695bfcd8aca3fd2d99e4b5f0deb4d1c02
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2022-06-28 22:29:34 -0500

    initial draft template
    Much more to come... getting variables ready

---
+++ main.py
@@ -0,0 +1,175 @@
+import streamlit as st
+import numpy as np
+from numpy import log as ln, mean
+
+st.title("Diabetes Care")
+st.write('*DRAFT*')
+st.write('*DRAFT - NOT FOR CLINICAL USE - Not validated across input ranges*')
+# Set page two three columns. First for inputs, 2nd for spacing, and 3rd for outputs and explanation.
+
+col1, col2, col3 = st.columns([2,1,4])
+
+with col1:
+    
+    # Pick a goal first!
+    
+    # Set A1c goals!
+    
+    st.markdown('### First, define your goals!')
+    goalhba1c = st.radio(
+        "Please select a target HbA1c.",
+        ('</= 6', '</= 6.5', '</= 7', '</= 7.5', '</= 8', '</= 8.5', 'Over 8.5'))
+    
+    if goalhba1c == '</= 6':
+        goalhba1c = 6.0
+    elif goalhba1c == '</= 6.5':
+        goalhba1c = 6.5
+    elif goalhba1c == '</= 7':
+        goalhba1c = 7.0
+    elif goalhba1c == '</= 7.5':
+        goalhba1c = 7.5
+    elif goalhba1c == '</= 8':
+        goalhba1c = 8.0
+    elif goalhba1c == '</= 8.5':
+        goalhba1c = 8.5
+    
+    st.markdown('### Next, enter details about your patient!')
+    
+    lasthba1c = st.slider("Select the last HbA1c", min_value= 3.0, max_value = 15.0, value = 7.0)
+    
+       
+    metformindose = st.radio(
+        "Metformin:",
+        ('Not taking', 'Below max dose', 'Max dose'))
+
+    age = st.slider("Age:", min_value=18, max_value=100, value =50)
+
+    # weight = st.number_input('Enter weight in pounds', min_value=75.0, max_value=400.0, value = 160., step = 1.)
+
+    weight = st.slider("Weight (pounds)", min_value=70, max_value=500, value =150)
+
+    height = st.slider("Height (inches)", min_value=36, max_value=96, value =65)
+
+    # Standard formula for BMI in pounds/inches. Height/weight are more often known than BMI by a patient.
+
+    bmi = 703 * weight / height**2
+
+    st.write('The BMI calculates to ', round(bmi,1))
+    
+    creatinine = st.slider("Last Creatinine", min_value=0.0, max_value = 15.0, value = 1.0)
+    
+    egfr = st.slider("Last eGFR", min_value= 0.0, max_value = 120.0, value = 59.0)
+
+    # Record whether the patient has HTN, smokes and has DM below. Sets to True. 
+
+    ishtn = st.checkbox('HTN: Select if treated for hypertension.')
+
+
+    issmoker = st.checkbox('Smoking: Select if the patient smokes.')
+
+
+    isCAD = st.checkbox('CAD: Select if the patient has CAD.')
+    
+
+    # Set sex and race according to algorithm options available. 
+
+    sex = st.radio(
+        "Please select a sex. (Note - limited to the options available in the published algorithm.) ",
+        ('female', 'male'))
+
+    race = st.radio(
+        "Please select a race. (Note - limited to the options available in the published algorithm.) ",
+        ('black', 'white'))
+
+    # Enter other values required by algorithm. Use strealit sliders where possible. 
+
+
+    sbp = st.slider("Current systolic blood pressure in mm Hg.", min_value=80, max_value=200, value =120)
+
+    glucose = st.slider("Fasting glucose mg/dL.", min_value=60, max_value=300, value =100)
+
+    tchol = st.slider("Total cholesterol in mg/dL.", min_value=80, max_value=300, value =160)
+
+    hdl = st.slider("HDL in mg/dL.", min_value=15, max_value=100, value =40)
+
+    qrs = st.slider("QRS duration in msec.", min_value=55, max_value=200, value =100)
+
+ 
+
+
+with col2:
+
+    # Provide some spacing through use of an empty column.
+
+    st.write(' ')
+
+if lasthba1c > goalhba1c:
+    nextstep1 = 'Increase Dose of Metformin!'
+    
+with col3:
+
+    # Provide diabetes considerations!
+
+
+    st.markdown('## *Diabetes Management Considerations:*')
+    
+    st.write('Your selected goal HbA1c: ' + str(goalhba1c))
+    
+    st.write('Your next steps: ' + str(nextstep1))
+    
+    
+    st.title(str(round(riskpct,2)) + ' %')
+
+    
+
+    st.markdown('### Summary of inputs:')
+
+    st.write('Age is: ' + str(age))
+    race
+    sex
+    
+    st.write('Cr: ' + str(creatinine) + ' mg/dL')
+    
+    st.write('eGFR: ' + str(egfr) + ' mL/min/m^2')
+    
+    if isCAD:
+        st.write("With CAD")
+    else:
+        st.write("Without CAD")
+
+    if ishtn:
+        st.write("With treated HTN")
+    else:
+        st.write("Without treated HTN")
+    
+    st.write('BMI is: ' + str(round(bmi,1)))
+
+    if issmoker:
+        st.write('Is a smoker')
+    else:
+        st.write('Is a non-smoker')
+    
+    st.write('SBP: ' + str(sbp) + ' mmHg')
+    st.write('Total cholesterol: ' + str(tchol) + ' mg/dL')
+    st.write('HDL: ' + str(hdl) + ' mg/dL')
+    st.write('QRS interval: ' + str(qrs) +' msec')
+
+    st.markdown('### Equation Details:')
+
+    
+    st.latex("Risk = 1 - S_{0}^{e^{(IndX - MeanCV)}}")
+
+    st.latex("S_{0} = survival\;(baseline)") 
+
+    st.latex("IndX = sum\;of\;(coefficient\;x\;value)") 
+
+    st.latex("MeanCV = Sex\;and\;race \;specific \;mean \;coefficient \;x \;value") 
+
+    st.markdown('*Reference*:')
+
+    st.markdown('Khan S, Ning H, Shah S, et al. 10-Year Risk Equations for Incident Heart Failure in the General Population. J Am Coll Cardiol. 2019 May, 73 (19) 2388-2397. https://doi.org/10.1016/j.jacc.2019.02.057')
+
+    st.markdown('Github URL: https://github.com/DrDavidL/pcp_hf')
+  
+
+
