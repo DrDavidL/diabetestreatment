975fc7ffc1bcf2b68c39e187189b6d032c0ab3f2
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2022-08-16 14:35:07 -0500

    standardized output headers.

--- main.py
+++ main.py
@@ -104,6 +104,8 @@ issmoker = st.sidebar.checkbox('Smoking: Select if the patient smokes.')
 
 isdiabetes = st.sidebar.checkbox('DM: Select if the patient has diabetes.', value = True)
 
+
+
 features = {1: ln(age), 2: ln(age)**2, 3: ln(tchol), 4: ln(age)*ln(tchol), 5: ln(hdl), 6: ln(age)*ln(hdl), 
             7: (is_htn + 0)*ln(sbp), 8: (is_htn +0)*ln(age)*ln(sbp), 9: ((not is_htn) + 0)*ln(sbp), 10: ((not is_htn) + 0)*ln(age)*ln(sbp), 11: (issmoker+0),
             12: ln(age)*(issmoker+0), 13: (isdiabetes+0) }
@@ -177,6 +179,14 @@ st.sidebar.write('Calculated BMI is: ', round(bmi,1), 'If only BMI known, adjust
 
 egfr = st.sidebar.slider("Last eGFR", min_value= 0.0, max_value = 120.0, value = 59.0)
 
+high_hypoglyc_risk = st.sidebar.radio(
+    "High Hypoglycemia Risk",
+    ('no', 'yes', 'unknown'), index = 2)
+
+high_insulin_resistance = st.sidebar.radio(
+    "High Level of Insulin Resistance",
+    ('no', 'yes', 'unknown'), index = 2)
+
 # Contraindications Section
 
 # Determine if metformin is contra-indicated to continue based on egfr
@@ -202,13 +212,13 @@ else:
 
 # Record whether the patient has HTN, smokes and has DM below. Sets to True. 
 
-is_cad = st.sidebar.checkbox('CAD: Select if the patient has CAD.')
+is_cad = st.sidebar.checkbox('CAD: Select if the patient had a prior cardiovascular event.')
 
-is_cva = st.sidebar.checkbox('CVA: Select if the patient had a stroke.')
+is_cva = st.sidebar.checkbox('CVA: Select if the patient had a prior cerebrovascular event.')
 
 is_ckd = st.sidebar.checkbox('CKD: Select if the patient has chronic kidney disease.')
 
-is_pad = st.sidebar.checkbox('PAD: Select if the patient has peripheral arterial disease.')
+is_pad = st.sidebar.checkbox('PAD: Select if the patient has symptomatic peripheral arterial disease.')
 
 is_hf = st.sidebar.checkbox('Heart Failure: Select if the patient has a hstory of heart failure.')
 
@@ -272,11 +282,11 @@ statindose = st.sidebar.radio(
     "Statin:",
     ('Not taking', 'Below max dose', 'Max dose', 'Contraindicated or intolerant'))
     
-aspirindose = st.sidebar.radio(
-    "Aspirin",
+anti_platelet_therapydose = st.sidebar.radio(
+    "anti_platelet_therapy",
     ('Not taking daily', 'Taking daily', 'Contraindicated or intolerant'))
         
-        
+
 
 
 
@@ -302,62 +312,62 @@ nextsteps = []
 
 if lasthba1c > goalhba1c and metformindose != 'Contraindicated or intolerant' and metformindose != 'Max dose' and metforminescalate == True:
     if metformindose == 'Not taking' and metformin_ok == True:
-        metformin_rec = 'Consider starting metformin. RATIONALE: Above goal Hba1c, not yet taking it, and no contraindications.' 
+        metformin_rec = 'Metformin: Consider starting. RATIONALE: Above goal Hba1c, not yet taking it, and no contraindications.' 
         nextsteps.append(metformin_rec)
     else:
         if metforminescalate == True:
-            metformin_rec = 'Consider increasing metformin dose. RATIONALE: Above goal Hba1c and submaximal metformin dose.'
+            metformin_rec = 'Metformin: Consider increasing dose. RATIONALE: Above goal Hba1c and submaximal metformin dose.'
             nextsteps.append(metformin_rec)   
     
 if metformin_ok == False:
     if metformindose == 'Max dose' or metformindose == 'Below max dose':
-        metformin_rec = "STOP METFORMIN -- check the eGFR value!"
+        metformin_rec = "Metformin: STOP the metformin -- check the eGFR value."
         nextsteps.append(metformin_rec)
         
 # GLP1 Logic
 
 if lasthba1c > goalhba1c and glp1agonistdose != 'Contraindicated or intolerant' and glp1agonistdose != 'Max dose':
     if glp1agonistdose == 'Not taking' and metformindose =='Max dose' and dpp4idose == 'Not taking':
-        glp1agonist_rec = 'Consider starting a GLP-1 agonist. RATIONALE: Above goal Hba1c, metformin is at maximal dose, and no use of DPP-4 inhibitor.' 
+        glp1agonist_rec = 'GLP-1 agonist: Consider starting. RATIONALE: Above goal Hba1c, metformin is at maximal dose, and no use of DPP-4 inhibitor.' 
         nextsteps.append(glp1agonist_rec)
         if is_ckd == True or egfr < 60:
-            glp1agonist_rec = "GLP-1 agonist also suggested for reason of CKD."
+            glp1agonist_rec = "GLP-1 agonist: Consider for reason of CKD."
             nextsteps.append(glp1agonist_rec)
     if glp1agonistdose == 'Below max dose':
-        glp1agonist_rec = "Consider increasing GLP-1 agonist dose. RATIONALE: Above goal Hba1c, submaximal dose, and no contraindications."
+        glp1agonist_rec = "GLP-1 agonist: Consider increasing GLP-1 agonist dose. RATIONALE: Above goal Hba1c, submaximal dose, and no contraindications."
         nextsteps.append(glp1agonist_rec)   
 if bmi >= 30:
-    glp1agonist_rec = "GLP-1 agonist also suggested for reason of obesity for BMI > 30."
+    glp1agonist_rec = "GLP-1 agonist: Consider for obesity for BMI > 30."
     nextsteps.append(glp1agonist_rec)
 
 if bmi >= 27 and bmi < 30 and isdiabetes == True:
-    glp1agonist_rec = "GLP-1 agonist also suggested since BMI >= 27 and diabetes is present."
+    glp1agonist_rec = "GLP-1 agonist: Consider adding since BMI >= 27 and diabetes is present."
     nextsteps.append(glp1agonist_rec)
         
 # SGLT2i logic
 
 if lasthba1c > goalhba1c and sglt2idose != 'Contraindicated or intolerant' and sglt2idose != 'Max dose' and sglt2i_ok == True:
     if sglt2idose == 'Not taking' and egfr > 29:
-        sglt2i_rec = 'Consider starting an SGLT2 inhibitor (CHECK specific SGLT2i for eGFR dosing guidance.) RATIONALE: Above goal Hba1c, not yet taking it, and no contraindications.' 
+        sglt2i_rec = 'SGLT2i: Consider starting an SGLT2 inhibitor (CHECK specific SGLT2i for eGFR dosing guidance.) RATIONALE: Above goal Hba1c, not yet taking it, and no contraindications.' 
         nextsteps.append(sglt2i_rec)
         if is_ckd == True or 30 < egfr < 60:
-            sglt2i_rec = "SGLT2 inhibitor also suggested for reason of CKD."
+            sglt2i_rec = "SGLT2i: SGLT2 inhibitor also suggested for reason of CKD."
             nextsteps.append(sglt2i_rec)
         if is_hf == True:
-            sglt2i_rec = "SGLT2 inhibitor also suggested for reason of heart failure."
+            sglt2i_rec = "SGLT2i: SGLT2 inhibitor also suggested for reason of heart failure."
             nextsteps.append(sglt2i_rec)
     if sglt2idose == 'Below max dose':
-        sglt2i_rec = "Consider increasing SGLT2 inhibitor dose. RATIONALE: Above goal Hba1c, submaximal dose, and no contraindications."
+        sglt2i_rec = "SGLT2i: Consider increasing SGLT2 inhibitor dose. RATIONALE: Above goal Hba1c, submaximal dose, and no contraindications."
         nextsteps.append(sglt2i_rec)   
 
 if sglt2idose == 'Not taking' and egfr > 29 and is_hf == True:
-    sglt2i_rec = 'Consider starting an SGLT2 inhibitor for heart failure independent of diabetes control. (CHECK specific SGLT2i for eGFR dosing guidance.) RATIONALE: Improved heart failure outcomes.' 
+    sglt2i_rec = 'SGLT2i: Consider starting an SGLT2 inhibitor for heart failure independent of diabetes control. (CHECK specific SGLT2i for eGFR dosing guidance.) RATIONALE: Improved heart failure outcomes.' 
     nextsteps.append(sglt2i_rec)    
 
 
 if sglt2i_ok == False:
     if sglt2idose == 'Max dose' or sglt2idose == 'Below max dose':
-        sglt2i_rec = "Check whether the specific SGLT2i should continue with current eGFR."
+        sglt2i_rec = "SGLT2i: Check whether the specific SGLT2i should continue with current eGFR."
         nextsteps.append(sglt2i_rec)
 
 # Here is DPP4 logic
@@ -365,10 +375,10 @@ if sglt2i_ok == False:
 
 if dpp4idose == "Below max dose" or dpp4idose == 'Max dose':
     if glp1agonistdose == "Below max dose" or glp1agonistdose == 'Max dose':
-        dpp4i_rec = "Stop DPP-4 inhibitor: The patient is on a GLP-1 agonist so there is no added benefit from the DPP-4 inhibitor."
+        dpp4i_rec = "DPP-4 inhibitor: Consider stopping. The patient is on a GLP-1 agonist so there is no added benefit from the DPP-4 inhibitor."
         nextsteps.append(dpp4i_rec)
     elif egfr < 59:
-        dpp4i_rec = "Renal function is abnormal. Ensure dpp4i dosing is appropriate for agent used."
+        dpp4i_rec = "DPP-4i: Renal function is abnormal. Ensure dpp4i dosing is appropriate for agent used."
 
 
 
@@ -377,27 +387,27 @@ if dpp4idose == "Below max dose" or dpp4idose == 'Max dose':
 if dpp4idose == "Not taking" or dpp4idose == 'Below max dose':
     if lasthba1c > goalhba1c and sglt2idose == 'Max dose' and metformindose == 'Max dose' and glp1agonistdose == 'Contraindicated or intolerant':
         if dpp4idose == 'Not taking':
-            dpp4i_rec = "Consider starting a DPP-4 inhibitor. RATIONALE: Metformin dose is max, SGLT2i dose is max and adding a GLP-1 agonist is not possible."
+            dpp4i_rec = "DPP-4i: Consider starting a DPP-4 inhibitor. RATIONALE: Metformin dose is max, SGLT2i dose is max and adding a GLP-1 agonist is not possible."
             nextsteps.append(dpp4i_rec)
         if dpp4i_rec == 'Below max dose':
-            dpp4i_rec = "Consider increasing the DPP-4 inhibitor dose. RATIONALE: Metformin dose is max, SGLT2i dose is max and adding a GLP-1 agonist is not possible."
+            dpp4i_rec = "DPP-4i: Consider increasing the DPP-4 inhibitor dose. RATIONALE: Metformin dose is max, SGLT2i dose is max and adding a GLP-1 agonist is not possible."
             nextsteps.append(dpp4i_rec)
 
 # Suflonylurea logic:
 
 if sulfonylureadose == 'Below max dose' or sulfonylureadose == 'Max dose':
     if egfr < 60:
-        sulfonylurea_rec = "Renal function is abnormal. Ensure sulfonylurea dosing is appropriate. Consider maximizing alternative agents better associated with improved outcomes." 
+        sulfonylurea_rec = "Sulfonylurea: Renal function is abnormal. Ensure sulfonylurea dosing is appropriate. Consider maximizing alternative agents better associated with improved outcomes." 
         nextsteps.append(sulfonylurea_rec)
     else:
-        sulfonylurea_rec = "Assess continued use of sulfonylurea in context of alternative agents better associated with improved outcomes." 
+        sulfonylurea_rec = "Sulfonylurea: Assess continued use of sulfonylurea in context of alternative agents better associated with improved outcomes." 
         nextsteps.append(sulfonylurea_rec)
 
 # Thiazolidinedione logic
 
 if thiazolidinedionedose == 'Below max dose' or thiazolidinedionedose == 'Max dose':
     if is_osteoporosis == True: 
-        thiazolidinedione_rec = 'Consider stopping thiazolidinediones in presence of osteoporosis.'
+        thiazolidinedione_rec = 'Thiazolidinedione: Consider stopping thiazolidinediones in presence of osteoporosis.'
         nextsteps.append(thiazolidinedione_rec)
         
 # Here is weight loss surgery discussion with different cutoff for Asian American.
@@ -411,7 +421,7 @@ else:
         considersurgery = True
 
 if considersurgery == True:
-    surgery_rec = 'Consider evaluation for weight loss surgery. RATIONALE: BMI above race-based cutoff for consideration.' 
+    surgery_rec = 'Surgery: Consider evaluation for weight loss surgery. RATIONALE: BMI above race-based cutoff for consideration.' 
     nextsteps.append(surgery_rec)
     
 # ACEI and ARB 
@@ -420,35 +430,48 @@ if considersurgery == True:
 if is_htn == True:
     if acearbdose == "Not taking":
         if egfr > 20 and egfr < 60:
-            acearb_rec = 'Consider addition of an ACEI or ARB for HTN management in CKD. Dose adjust for eGFR per individual agent.'
+            acearb_rec = 'ACEI/ARB: Consider addition of an ACEI or ARB for HTN management in CKD. Dose adjust for eGFR per individual agent.'
             nextsteps.append(acearb_rec)
             
 if is_proteinuria == True and isdiabetes == True:
     if acearbdose == "Not taking":
         if egfr > 20 and egfr < 60:
-            acearb_rec = 'Consider addition of an ACEI or ARB given presence of proteinuria and diabetes. Dose adjust for eGFR per individual agent.'
+            acearb_rec = 'ACEI/ARB: Consider addition of an ACEI or ARB given presence of proteinuria and diabetes. Dose adjust for eGFR per individual agent.'
             nextsteps.append(acearb_rec)
 
-# Aspirin Recs
+# anti-platelet therapy Recs
 
-if ten_yr_risk > 10 and aspirindose == 'Not taking daily' and is_cad == False and 39 < age < 60:
-    aspirin_rec = 'Discuss with patient regarding daily aspirin for primary ASCVD prevention. The 10 year ASCVD risk is above 10 AND age is 40 - 59.'
-    nextsteps.append(aspirin_rec)
+if ten_yr_risk > 10 and anti_platelet_therapydose == 'Not taking daily' and is_cad == False and 39 < age < 60 and is_pad == False and is_cva == False:
+    anti_platelet_therapy_rec = 'Anti-platelet therapy: Discuss with patient regarding daily anti-platelet therapy for primary ASCVD prevention. The 10 year ASCVD risk is above 10 AND age is 40 - 59.'
+    nextsteps.append(anti_platelet_therapy_rec)
     
-if aspirindose == 'Not taking daily' and is_cad == True:
-    aspirin_rec = 'Given CAD, consider adding aspirin for secondary ASCVD prevention.'
-    nextsteps.append(aspirin_rec)
-
+if anti_platelet_therapydose == 'Not taking daily':
+    if is_cad == True:
+        anti_platelet_therapy_rec = 'Anti-platelet therapy: Given CAD event history, consider adding anti-platelet therapy for secondary prevention.'
+        nextsteps.append(anti_platelet_therapy_rec)
+    if is_cva == True:
+        anti_platelet_therapy_rec = 'Anti-platelet therapy: Given CVA event history, consider adding anti-platelet therapy for secondary prevention.'
+        nextsteps.append(anti_platelet_therapy_rec)
+    if is_pad == True:
+        anti_platelet_therapy_rec = 'Anti-platelet therapy: Given symptomatic PAD, consider adding anti-platelet therapy for reduced morbidity.'
+        nextsteps.append(anti_platelet_therapy_rec)
 # Statin recs
     
 if ten_yr_risk > 10 and statindose == 'Not taking':
     if is_cad == False: 
-        statin_rec = 'Consider a statin for primary ASCVD prevention. The 10 year ASCVD risk is above 10.'
+        statin_rec = 'Statin: Consider a statin for primary ASCVD prevention. The 10 year ASCVD risk is above 10.'
         nextsteps.append(statin_rec)
         
-if is_cad == True and statindose == 'Not taking':  
-    statin_rec = 'Given CAD, consider adding a statin for secondary ASCVD prevention.'
-    nextsteps.append(statin_rec)
+if statindose == 'Not taking':
+    if is_cad == True:  
+        statin_rec = 'Statin: Given CAD event history, consider adding a statin for secondary ASCVD prevention.'
+        nextsteps.append(statin_rec)
+    if is_cva == True:  
+        statin_rec = 'Statin: Given CVA event history, consider adding a statin for secondary ASCVD prevention.'
+        nextsteps.append(statin_rec)
+    if is_pad == True:  
+        statin_rec = 'Statin: Given symptomatic PAD, consider adding a statin for secondary ASCVD prevention.'
+        nextsteps.append(statin_rec)
 
 # Provide diabetes care considerations!
 
@@ -509,7 +532,7 @@ st.write('Additional Medications of Interest:')
 
 st.markdown(' - ***ACEi or ARB:***  ' + acearbdose)
 st.markdown(' - ***Statin:***  ' + statindose)
-st.markdown(' - ***Aspirin:***  ' + aspirindose)
+st.markdown(' - ***anti_platelet_therapy:***  ' + anti_platelet_therapydose)
 
 
 # st.markdown('### Summary of inputs:')
